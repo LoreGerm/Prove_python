e3d809eab9b9a675d1e86a619b86fea695851e21
LoreGerm <lorenzogermano146@gmail.com>
2022-03-19 14:39:16 +0100

    .

--- Django_prova/Form/forms.py
+++ Django_prova/Form/forms.py
@@ -1,10 +1,18 @@
 
 from django import forms
+from matplotlib import widgets
 from Form.models import utente
 
 class FormMsg(forms.ModelForm):
     class Meta:
         model = utente
         fields = '__all__'
+        
+    widgets = {
+        'nome': forms.TextInput(attrs={'class': 'form-control'}),
+        'cognome': forms.TextInput(attrs={'class': 'form-control'}),
+        'email': forms.TextInput(attrs={'class': 'form-control'}),
+        'messaggio': forms.TextInput(attrs={'class': 'form-control'}),
+    }
 
     
